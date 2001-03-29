15068f925ee3c3063fe05978ca51db3dfb19fb43
justin <justin>
2001-03-29 15:05:11 +0000

    JC: First blush at the basic toolbar. Much simpler. Only has the 3 nav buttons. Other toolbars with features to follow

--- src/java/org/j3d/ui/navigation/NavigationToolbar.java
+++ src/java/org/j3d/ui/navigation/NavigationToolbar.java
@@ -14,180 +14,138 @@ package org.j3d.ui.navigation;
 
 // Standard imports
 import  javax.swing.*;
-import  java.text.*;
-import  java.net.*;
-import  java.util.*;
-import  java.awt.*;
-import  java.awt.event.*;
-import  javax.media.j3d.*;
-import  javax.vecmath.*;
+
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Application specific imports
+import org.j3d.util.ImageLoader;
 
 /**
-    This is the toolbar for all navigation and view manipulation commands.
-    @author <a href="http://www.geocities.com/seregi/index.html">Laszlo Seregi</a><br>
+ * A toolbar for all navigation commands.
+ * <p>
+ *
+ * The toolbar offers three buttons representing the navigation states. As this
+ * panel also implements the state listener, these will change in response to
+ * the mouse changing. Clicking these buttons will send the appropriate event
+ * back to it's registered listener.
+ *
+ * @author <a href="http://www.geocities.com/seregi/index.html">Laszlo Seregi</a><br>
+ *    Updated for j3d.org by Justin Couch
+ * @version $Revision: 1.2 $
  */
 public class NavigationToolbar extends JPanel
-    implements NavigationStateListener
+    implements ActionListener, NavigationStateListener
 {
+    // Constants for images
+
+    /** The name of the file for the pan cursor image */
+    private static final String PAN_BUTTON = "images/navigation/ButtonPan.gif";
+
+    /** The name of the file for the tilt cursor image */
+    private static final String TILT_BUTTON = "images/navigation/ButtonTilt.gif";
+
+    /** The name of the file for the walk cursor image */
+    private static final String WALK_BUTTON = "images/navigation/ButtonWalk.gif";
+
+    // Local variables
+
     /** The current navigation state either set from us or externally */
     private int navigationState = WALK_STATE;
 
     /** An observer for navigation state change information */
     private NavigationStateListener navigationListener;
 
-/*
-    private     Ddd                         ddd;
-    private     JComboBox                   jComboBoxViews          =   new JComboBox       ();
-    public      ButtonGroup                 buttonGroupNavigation   =   new ButtonGroup();
-    public      ButtonGroup                 buttonGroupMode         =   new ButtonGroup();
-    public      JToggleButton               buttonWalk              =   new JToggleButton   (new ImageIcon(ClassLoader.getSystemResource("images/ButtonWalk.gif")));
-    public      JToggleButton               buttonTilt              =   new JToggleButton   (new ImageIcon(ClassLoader.getSystemResource("images/ButtonTilt.gif")));
-    public      JToggleButton               buttonPan               =   new JToggleButton   (new ImageIcon(ClassLoader.getSystemResource("images/ButtonPan.gif")));
-    public      JButton                     buttonViewForward       =   new JButton         (new ImageIcon(ClassLoader.getSystemResource("images/ButtonForward.gif")));
-    public      JButton                     buttonViewBackward      =   new JButton         (new ImageIcon(ClassLoader.getSystemResource("images/ButtonBack.gif")));
-    public      JButton                     buttonStraighten        =   new JButton         (new ImageIcon(ClassLoader.getSystemResource("images/ButtonHome.gif")));
-    public      JPopupMenu                  popup                   =   new JPopupMenu      ("Navigation3D");
-    public      Cursor                      cursorSelection         =   null;
-    private     Canvas3D                    dddCanvas3D;
-    private     View                        view;
-    private     TransformGroup              tgView;
-    //===============================================================
+    /** Button group holding the navigation state buttons */
+    private ButtonGroup navStateGroup;
 
-    public DddToolBar(Ddd ddd,Canvas3D dddCanvas3D,View view,TransformGroup tgView)
+    /** Button representing the walk navigation state */
+    private JToggleButton walkButton;
+
+    /** Button representing the tilt navigation state */
+    private JToggleButton tiltButton;
+
+    /** Button representing the pan navigation state */
+    private JToggleButton panButton;
+
+    /**
+     * Create a new horizontal navigation toolbar with an empty list of
+     * viewpoints.
+     */
+    public NavigationToolbar()
     {
-        this.ddd                =   ddd;
-        this.dddCanvas3D        =   dddCanvas3D;
-        this.view               =   view;
-        this.tgView             =   tgView;
-        setFloatable(false);
-        // buttonGroupNavigation:
-        buttonGroupNavigation.add(buttonWalk);
-        buttonGroupNavigation.add(buttonTilt);
-        buttonGroupNavigation.add(buttonPan);
-        // setMargin:
-        buttonWalk.setMargin            (new Insets(0,0,0,0));
-        buttonTilt.setMargin            (new Insets(0,0,0,0));
-        buttonPan.setMargin            (new Insets(0,0,0,0));
-        buttonViewForward.setMargin     (new Insets(0,0,0,0));
-        buttonViewBackward.setMargin    (new Insets(0,0,0,0));
-        buttonStraighten.setMargin      (new Insets(0,0,0,0));
-        jComboBoxViews.setMinimumSize   (new Dimension(60,10));
-        // addActionListener:
-        buttonWalk          .addActionListener(new actionListener());
-        buttonTilt          .addActionListener(new actionListener());
-        buttonPan          .addActionListener(new actionListener());
-        buttonViewForward   .addActionListener(new actionListener());
-        buttonViewBackward  .addActionListener(new actionListener());
-        buttonStraighten    .addActionListener(new actionListener());
-        jComboBoxViews      .addActionListener(new actionListener());
-        //  setToolTipText:
-        buttonWalk          .setToolTipText("Walk");
-        buttonTilt          .setToolTipText("Tilt");
-        buttonPan           .setToolTipText("Pan");
-        buttonViewForward   .setToolTipText("Next Viewpoint");
-        buttonViewBackward  .setToolTipText("Previous Viewpoint");
-        buttonStraighten    .setToolTipText("Straighten");
-        jComboBoxViews      .setToolTipText("Views");
-        //  add the Components to the toolbar:
-        add(buttonViewBackward);
-        add(jComboBoxViews);
-        add(buttonViewForward);
-        addSeparator();
-        add(buttonWalk);
-        add(buttonTilt);
-        add(buttonPan);
-        addSeparator();
-        add(buttonStraighten);
-        addSeparator();
+        this(null, true);
     }
-    //===============================================================================================================
-    public void initialize(BranchGroup bgArena,Scene[] sceneArenas)
+
+    /**
+     * Create a new navigation toolbar with an empty list of viewpoints but
+     * controllable direction for the buttons.
+     *
+     * @param horizontal True to lay out the buttons horizontally
+     */
+    public NavigationToolbar(boolean horizontal)
     {
-        if(bgArena!=null && sceneArenas!=null)
-        {
-            TransformGroup[]    tgLandmark_;
-            Transform3D         t3dLandmark     =   new Transform3D();
-            //  Views:
-            jComboBoxViews.removeAllItems();
-            for(int t=0;t<sceneArenas.length;t++)
-            {
-                tgLandmark_=sceneArenas[t].getViewGroups();
-                if(tgLandmark_==null)break;
-                for(int n=0;n<tgLandmark_.length;n++)
-                {
-                    jComboBoxViews.addItem(new UserData("Landmark",n,tgLandmark_[n]));
-                    // Select the first landmark to start with:
-                    if(n==0)
-                    {
-                        tgLandmark_[n].getTransform(t3dLandmark);
-                        tgView.setTransform(t3dLandmark);
-                    }
-                }
-            }
-        }
-        jComboBoxViews.addItem(new UserData("Goto ",0,new TransformGroup()));
-        //  Actions:
-        buttonWalk.doClick();
+        this(null, horizontal);
     }
-    //==========================================================================================================
-    // This Class catches all the toolbar events.
-    private class actionListener implements ActionListener
+
+    /**
+     * Create a new navigation toolbar that has the given list of viewpoints
+     * to be displayed. List may be null and may be changed at a later date.
+     * The buttons will be laid out horizontally
+     *
+     * @param vps The list of viewpoints to use
+     */
+    public NavigationToolbar(ViewpointData[] vps)
     {
-        Object  o;
-        int     selection0=-1;
-        public void actionPerformed(ActionEvent e)
-        {
-            o=e.getSource();
-            //  Radio buttons:
-            if(buttonWalk.isSelected()) cursorSelection=ddd.dddMouseKeyboardInput.cursorWalk;
-            if(buttonTilt.isSelected()) cursorSelection=ddd.dddMouseKeyboardInput.cursorTilt;
-            if(buttonPan.isSelected()) cursorSelection=ddd.dddMouseKeyboardInput.cursorPan;
-            ddd.dddMouseKeyboardInput.setCursor(cursorSelection);
-            //  View controls:
-            if(o==buttonViewBackward)   {setViewSelection(-1,true);}
-            if(o==buttonViewForward)    {setViewSelection(+1,true);}
-            if(o==jComboBoxViews)       {setViewSelection(((JComboBox)o).getSelectedIndex(),false);}
-            //  buttonStraighten:
-            if(o==buttonStraighten)
-            {
-                Transform3D t3d             =   new Transform3D();
-                Point3d     direction       =   new Point3d(0,0,-1);
-                Vector3d    vUp             =   new Vector3d(0,1,0);
-                Vector3d    v3d     =   new Vector3d();
-                tgView.getTransform(t3d);
-                t3d.get(v3d);
-                Point3d     eye     =   new Point3d(v3d);
-                tgView.getTransform(t3d);
-                t3d.transform(direction);
-                direction.y=eye.y;
-                t3d.lookAt(eye,direction,vUp);
-                t3d.invert();
-                ddd.dddMouseKeyboardInput.transitionView.transitionTo(tgView,t3d,1000);
-            }
-        }
-        //======================================================================================================
-        public void setViewSelection(int i,boolean relative)
-        {
-            int selection=jComboBoxViews.getSelectedIndex();
-            if(selection==-1)return;
-            int lastItem=jComboBoxViews.getItemCount()-1;
-            if(relative)selection=selection+i;
-            else        selection=i;
-            if(selection>lastItem)selection=0;
-            if(selection<0)selection=lastItem;
-            jComboBoxViews.setSelectedIndex(selection);
-            if(relative)return;// return because otherwise it will be called twice
-            selection0=selection;
-            //  Set the viewpoint:
-            Transform3D t3d=new Transform3D();
-            //  ComboBox
-            //System.out.println("selection"+selection);
-            UserData userObject=(UserData)jComboBoxViews.getSelectedItem();
-            userObject.tg.getTransform(t3d);
-            ddd.dddMouseKeyboardInput.transitionView.transitionTo(tgView,t3d,1000);
-        }
+        this(vps, true);
     }
-*/
+
+    /**
+     * Create a new navigation toolbar that has the given list of viewpoints
+     * to be displayed. List may be null and may be changed at a later date.
+     *
+     * @param horizontal True to lay out the buttons horizontally
+     * @param vps The list of viewpoints to use
+     */
+    public NavigationToolbar(ViewpointData[] vps, boolean horizontal)
+    {
+        if(horizontal)
+            setLayout(new GridLayout(1, 3));
+        else
+            setLayout(new GridLayout(3, 1));
+
+        navStateGroup = new ButtonGroup();
+
+        Icon icon = ImageLoader.loadIcon(WALK_BUTTON);
+        walkButton = new JToggleButton(icon);
+        walkButton.setMargin(new Insets(0,0,0,0));
+        walkButton.setToolTipText("Walk");
+        walkButton.addActionListener(this);
+        navStateGroup.add(walkButton);
+        add(walkButton);
+
+        icon = ImageLoader.loadIcon(TILT_BUTTON);
+        tiltButton = new JToggleButton(icon);
+        tiltButton.setMargin(new Insets(0,0,0,0));
+        tiltButton.setToolTipText("Tilt");
+        tiltButton.addActionListener(this);
+        navStateGroup.add(tiltButton);
+        add(tiltButton);
+
+        icon = ImageLoader.loadIcon(PAN_BUTTON);
+        panButton = new JToggleButton(icon);
+        panButton.setMargin(new Insets(0,0,0,0));
+        panButton.setToolTipText("Pan");
+        panButton.addActionListener(this);
+        navStateGroup.add(panButton);
+        add(panButton);
+    }
+
+    //----------------------------------------------------------
+    // Local public methods
+    //----------------------------------------------------------
 
     /**
      * Set the listener for navigation state change notifications. By setting
@@ -198,6 +156,39 @@ public class NavigationToolbar extends JPanel
     public void setNavigationStateListener(NavigationStateListener l)
     {
         navigationListener = l;
+    }
+
+    //----------------------------------------------------------
+    // Methods required by the ActionListener
+    //----------------------------------------------------------
+
+    /**
+     * Process an action event on one of the buttons.
+     *
+     * @param evt The event that caused this method to be called
+     */
+    public void actionPerformed(ActionEvent evt)
+    {
+        Object src = evt.getSource();
+
+        if(src == walkButton)
+        {
+            navigationState = WALK_STATE;
+            if(navigationListener != null)
+                navigationListener.setNavigationState(navigationState);
+        }
+        else if(src == tiltButton)
+        {
+            navigationState = TILT_STATE;
+            if(navigationListener != null)
+                navigationListener.setNavigationState(navigationState);
+        }
+        else if(src == panButton)
+        {
+            navigationState = PAN_STATE;
+            if(navigationListener != null)
+                navigationListener.setNavigationState(navigationState);
+        }
     }
 
     //----------------------------------------------------------
@@ -236,4 +227,5 @@ public class NavigationToolbar extends JPanel
     {
         return navigationState;
     }
+
 }
