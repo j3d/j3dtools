27635aa7b549b557360f6a992475d872f4241a85
justin <justin>
2001-05-31 12:22:57 +0000

    JC: Moved all the definitions of the state inforamtion into a separate interface NavigationState rather than use the listener for all of these

--- src/java/org/j3d/ui/navigation/NavigationState.java
+++ src/java/org/j3d/ui/navigation/NavigationState.java
@@ -19,27 +19,29 @@ package org.j3d.ui.navigation;
 // none
 
 /**
- * A listener interface used to communicate changes in the navigation state
- * from one handler to another.
+ * A collection of navigation state information constants.
  * <p>
  *
  * @author Justin Couch
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.1 $
  */
-public interface NavigationStateListener  extends NavigationState
+public interface NavigationState
 {
-    /**
-     * Notification that the panning state has changed to the new state.
-     *
-     * @param state One of the state values declared here
-     */
-    public void setNavigationState(int state);
+    /** The navigation state is Walking */
+    public static int WALK_STATE = 1;
 
-    /**
-     * Callback to ask the listener what navigation state it thinks it is
-     * in.
-     *
-     * @return The state that the listener thinks it is in
-     */
-    public int getNavigationState();
+    /** The navigation state is Tilt */
+    public static int TILT_STATE = 2;
+
+    /** The navigation state is Panning */
+    public static int PAN_STATE = 3;
+
+    /** The navigation state is Flying */
+    public static int FLY_STATE = 4;
+
+    /** The navigation state is Examine */
+    public static int EXAMINE_STATE = 5;
+
+    /** The navigation state is such that there is no navigation */
+    public static int NO_STATE = 0;
 }
