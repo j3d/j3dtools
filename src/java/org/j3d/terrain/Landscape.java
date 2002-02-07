8ee15f550abe456a86f00080549d5a0b3ff0cd2a
justin <justin>
2002-02-07 02:28:41 +0000

    JC:
    Bunch of updates to start streamlining the code into the rest of the repository
    code.
    
    - Added a new convenience method setView(Transform3D) to allow a custom view
      to be set without needing to deconstruct to vectors.
    
    - Position information to setView() is now a Tuple3f rather than Point3f as
      some code uses a Point, others use Vector. Either way, they are
      interchangable (if someone wants to set a Color3f, well let them be that
      stupid). Other code is modified appropriately for this change.
    
    - Extended the FrameUpdateListener from the navigation package for
      convenience. I figure that this class is going to be used with the nav
      code quite a bit (particularly once we include height map handling directly
      into the nav code) so it is useful to have it here to save many people
      re-implementing the same thing all the time.
    
    - Documented how to extend the class to provide a specific culling
      algorithm.
    
    - Removed Sun copyright notice because this file doesn't look anything like
      their original code now. Kept attribution to Paul Byrne though as he did
      heavily influence the basic structure.

--- src/java/org/j3d/terrain/Landscape.java
+++ src/java/org/j3d/terrain/Landscape.java
@@ -7,44 +7,17 @@
  *
  ****************************************************************************/
 
-/*
- * @(#)Landscape.java 1.3 02/01/10 09:29:16
- *
- * Copyright (c) 2000-2002 Sun Microsystems, Inc.  All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *    -Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *    -Redistribution in binary form must reproduct the above copyright notice,
- *     this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- * Neither the name of Sun Microsystems, Inc. or the names of contributors may
- * be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * This software is provided "AS IS," without a warranty of any kind. ALL
- * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY
- * IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR
- * NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN AND ITS LICENSORS SHALL NOT BE
- * LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
- * OR DISTRIBUTING THE SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS
- * LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT,
- * INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
- * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF
- * OR INABILITY TO USE SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGES.
- *
- * You acknowledge that Software is not designed,licensed or intended for use in
- * the design, construction, operation or maintenance of any nuclear facility.
- */
 package org.j3d.terrain;
 
-import javax.vecmath.Point3f;
+// Standard imports
+import javax.media.j3d.Transform3D;
+
+import javax.vecmath.Matrix3f;
+import javax.vecmath.Tuple3f;
 import javax.vecmath.Vector3f;
+
+// Application specific imports
+import org.j3d.ui.navigation.FrameUpdateListener;
 
 /**
  * Representation of a piece of rendered terrain data.
@@ -54,17 +27,54 @@ import javax.vecmath.Vector3f;
  * moves about the virtual environment. This instance does not need to maintain
  * all the polygons on the screen at any one time, but may control them as
  * needed.
+ * <p>
  *
- * @author Paul Byrne, Justin Couch
+ * This object is independent of the culling algorithm. It represents something
+ * that can be placed in a scenegraph and have view information passed to it
+ * without the need to know the specific algorithm in use. To implement a
+ * specific algorithm (eg ROAM) you would extend this class and implement the
+ * {@link #setView(Tuple3f, Vector3f)} method. Every time that the scene
+ * changes, you will be notified by this method. That means you should perform
+ * any culling/LOD and update the scene graph at this point. This will be
+ * called at most once per frame.
+ * <p>
+ *
+ * For convenience, this class also implements {@link FrameUpdateListener} from
+ * the {@link org.j3d.ui.navigation} package so that you can have fast, quick
+ * navigation implementation in your code. If you wish to use your own custom
+ * user input code, then there is no penalty for doing so. Simply call one of
+ * the <code>setView()</code> methods directly with the transformation
+ * information.
+ *
+ * @author Justin Couch, based on original ideas from Paul Byrne
  * @version
  */
 public abstract class Landscape extends javax.media.j3d.BranchGroup
+    implements FrameUpdateListener
 {
     /** The current viewing frustum that is seeing the landscape */
     protected ViewFrustum landscapeView;
 
     /** Raw terrain information to be rendered */
     protected TerrainData terrainData;
+
+    /**
+     * Temporary variable to hold the position information extracted from
+     * the full transform class.
+     */
+    private Vector3f tmpPosition;
+
+    /**
+     * Temporary variable to hold the orientation information extracted from
+     * the matrix class.
+     */
+    private Vector3f tmpOrientation;
+
+    /**
+     * Temporary variable to hold the orientation matrix extracted from
+     * the full transform class.
+     */
+    private Matrix3f tmpMatrix;
 
     /**
      * Create a new Landscape with the set view and data. If either are not
@@ -84,7 +94,41 @@ public abstract class Landscape extends javax.media.j3d.BranchGroup
 
         terrainData = data;
         landscapeView = view;
+
+        tmpPosition = new Vector3f();
+        tmpOrientation = new Vector3f();
+        tmpMatrix = new Matrix3f();
     }
+
+    //----------------------------------------------------------
+    // Methods required by FrameUpdateListener
+    //----------------------------------------------------------
+
+    /**
+     * The transition from one point to another is completed. Use this to
+     * update the transformation.
+     *
+     * @param t3d The position of the final viewpoint
+     */
+    public void transitionEnded(Transform3D t3d)
+    {
+        setView(t3d);
+    }
+
+    /**
+     * The frame has just been updated with the latest view information.
+     * Update the landscape rendered values now.
+     *
+     * @param t3d The position of the viewpoint now
+     */
+    public void viewerPositionUpdated(Transform3D t3d)
+    {
+        setView(t3d);
+    }
+
+    //----------------------------------------------------------
+    // Local methods
+    //----------------------------------------------------------
 
     /**
      * Set the current viewing direction for the user. The user is located
@@ -94,5 +138,23 @@ public abstract class Landscape extends javax.media.j3d.BranchGroup
      * @param position The position the user is in the virtual world
      * @param direction The orientation of the user's gaze
      */
-    public abstract void setView(Point3f position, Vector3f direction);
+    public abstract void setView(Tuple3f position, Vector3f direction);
+
+    /**
+     * Set the current view location information based on a transform matrix.
+     * Only the position and orientation information are extracted from this
+     * matrix. Any shear or scale is ignored. Effectively, this transform
+     * should be the view transform (particularly if you are using navigation
+     * code from this codebase in the {@link org.j3d.ui.navigation} package.
+     *
+     * @param t3d The transform to use as the view position
+     */
+    public void setView(Transform3D t3d)
+    {
+        t3d.get(tmpMatrix, tmpPosition);
+        tmpOrientation.set(0, 0, -1);
+        tmpMatrix.transform(tmpOrientation);
+
+        setView(tmpPosition, tmpOrientation);
+    }
 }
