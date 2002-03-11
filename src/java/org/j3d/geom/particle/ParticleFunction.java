ef43b288facc88cc5c9c41cd2bb2a99fc7b4ff13
daniel <daniel>
2002-03-11 08:44:12 +0000

    DCS - added a file that I forgot at the previous check-in. Sorry!

---
+++ src/java/org/j3d/geom/particle/ParticleFunction.java
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ *                        Copyright (c) 2001 Daniel Selman
+ *                               Java Source
+ *
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
+ *
+ ****************************************************************************/
+
+package org.j3d.geom.particle;
+
+/**
+ * ParticleFunction is the basic interface for
+ * functions that can modify the fields of a Particle.
+ * Some movement functions will modify the force/energy of
+ * a particle while others will convert the energy into
+ * position deltas.
+ *
+ * @author Daniel Selman
+ * @version $Revision: 1.1 $
+ */
+public interface ParticleFunction
+{
+    public boolean apply( Particle particle );
+    public boolean onUpdate( ParticleSystem ps );
+}
