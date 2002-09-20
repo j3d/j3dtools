21446e45f0938d6f0115bbd610d46b71afcb1b4c
justin <justin>
2002-09-20 18:40:01 +0000

    JC: added methods to compute normals in general for all patches. Some minor bugs with the corners not generating the right normal direction and the last row has a light spot. Will fix

--- src/java/org/j3d/geom/spline/PatchGenerator.java
+++ src/java/org/j3d/geom/spline/PatchGenerator.java
@@ -10,7 +10,7 @@
 package org.j3d.geom.spline;
 
 // Standard imports
-// none
+import javax.vecmath.Vector3f;
 
 // Application specific imports
 import org.j3d.geom.GeometryData;
@@ -30,7 +30,7 @@ import org.j3d.geom.UnsupportedTypeException;
  * average between the adjacent edges.
  *
  * @author Justin Couch
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public abstract class PatchGenerator extends GeometryGenerator
 {
@@ -50,13 +50,25 @@ public abstract class PatchGenerator extends GeometryGenerator
     protected float[][] patchNormals;
 
     /** The texture coordinate for each point on the patch. */
-    protected float[][] patchCoordinates;
+    protected float[] patchTexcoords;
 
     /** The number of patch coordinates in depth */
     protected int numPatchValues;
 
+    /** The number of patch coordinates in depth */
+    protected int numNormalValues;
+
+    /** The number of patch coordinates in depth */
+    protected int numTexcoordValues;
+
     /** Flag indicating base values have changed */
     protected boolean patchChanged;
+
+    /** Flag indicating base values have changed */
+    protected boolean normalsChanged;
+
+    /** Flag indicating base values have changed */
+    protected boolean texCoordsChanged;
 
     /**
      * The number of sections used around the patch. Assumes square
@@ -72,6 +84,8 @@ public abstract class PatchGenerator extends GeometryGenerator
         numWidthControlPoints = 0;
         numDepthControlPoints = 0;
         patchChanged = true;
+        normalsChanged = true;
+        texCoordsChanged = true;
     }
 
     /**
@@ -87,8 +101,11 @@ public abstract class PatchGenerator extends GeometryGenerator
         if(facets < 3)
             throw new IllegalArgumentException("Number of facets is < 3");
 
-        if(facetCount != facets)
+        if(facetCount != facets) {
             patchChanged = true;
+            normalsChanged = true;
+            texCoordsChanged = true;
+        }
 
         facetCount = facets;
     }
@@ -147,6 +164,8 @@ public abstract class PatchGenerator extends GeometryGenerator
         numDepthControlPoints = min_length / 3;
 
         patchChanged = true;
+        normalsChanged = true;
+        texCoordsChanged = true;
     }
 
     /**
@@ -204,8 +223,9 @@ public abstract class PatchGenerator extends GeometryGenerator
         numDepthControlPoints = numDepth;
 
         patchChanged = true;
+        normalsChanged = true;
+        texCoordsChanged = true;
     }
-
 
     /**
      * Get the number of vertices that this generator will create for the
@@ -253,7 +273,259 @@ public abstract class PatchGenerator extends GeometryGenerator
      * patch. It assumes that the patch has been regenerated just before
      * this call.
      */
-    protected regenerateNormals()
+    protected void regenerateNormals()
     {
+        if(!normalsChanged)
+            return;
+
+        normalsChanged = false;
+        numNormalValues = numPatchValues;
+
+        if((patchNormals == null) ||
+           (patchNormals.length <= facetCount) ||
+           (patchNormals[0].length < numNormalValues))
+        {
+            patchNormals = new float[facetCount + 1][numNormalValues];
+        }
+
+        Vector3f norm;
+        int count = 0;
+        int i, j;
+
+        // The first edge
+        // corner point - normal based on only that face
+        norm = createFaceNormal(patchCoordinates, 1, 0, 0, 0, 0, 3);
+
+        patchNormals[0][count++] = norm.x;
+        patchNormals[0][count++] = norm.y;
+        patchNormals[0][count++] = norm.z;
+
+        for(i = 1; i < facetCount; i++)
+        {
+            norm = calcSideAverageNormal(0, count,
+                                         0, count + 3,
+                                         1, count,
+                                         0, count - 3);
+
+            patchNormals[0][count++] = norm.x;
+            patchNormals[0][count++] = norm.y;
+            patchNormals[0][count++] = norm.z;
+        }
+
+        // Last corner point of the first row
+        norm = createFaceNormal(patchCoordinates,
+                                0, count,
+                                0, count,
+                                1, count - 3);
+
+        patchNormals[0][count++] = norm.x;
+        patchNormals[0][count++] = norm.y;
+        patchNormals[0][count++] = norm.z;
+
+        // Now, process all of the internal points
+        for(i = 1; i < facetCount; i++)
+        {
+            count = 0;
+            norm = calcSideAverageNormal(i, count,
+                                         i - 1, count,
+                                         i, count + 3,
+                                         i + 1, count);
+
+            patchNormals[i][count++] = norm.x;
+            patchNormals[i][count++] = norm.y;
+            patchNormals[i][count++] = norm.z;
+
+            for(j = 1; j < facetCount; j++)
+            {
+
+                norm = calcQuadAverageNormal(i, count,
+                                             i, count + 3,
+                                             i + 1, count,
+                                             i, count - 3,
+                                             i - 1, count);
+
+                patchNormals[i][count++] = norm.x;
+                patchNormals[i][count++] = norm.y;
+                patchNormals[i][count++] = norm.z;
+            }
+
+            // Last point of the row
+            norm = calcSideAverageNormal(i, count,
+                                         i + 1, count,
+                                         i, count - 3,
+                                         i - 1, count);
+
+            patchNormals[i][count++] = norm.x;
+            patchNormals[i][count++] = norm.y;
+            patchNormals[i][count++] = norm.z;
+        }
+
+        // The last edge
+        // corner point - normal based on only that face
+        count = 0;
+        norm = createFaceNormal(patchCoordinates,
+                                facetCount - 1, count,
+                                facetCount - 1, count,
+                                facetCount, count + 3);
+
+        patchNormals[facetCount][count++] = norm.x;
+        patchNormals[facetCount][count++] = norm.y;
+        patchNormals[facetCount][count++] = norm.z;
+
+        for(i = 1; i < facetCount; i++)
+        {
+            norm = calcSideAverageNormal(i, count,
+                                         i, count - 3,
+                                         i - 1, count,
+                                         i, count + 3);
+
+            patchNormals[facetCount][count++] = norm.x;
+            patchNormals[facetCount][count++] = norm.y;
+            patchNormals[facetCount][count++] = norm.z;
+        }
+
+        // Last corner point of the first row
+        norm = createFaceNormal(patchCoordinates,
+                                facetCount, count,
+                                facetCount, count - 3,
+                                facetCount - 1, count);
+
+        patchNormals[facetCount][count++] = norm.x;
+        patchNormals[facetCount][count++] = norm.y;
+        patchNormals[facetCount][count++] = norm.z;
+    }
+
+    /**
+     * Convenience method to calculate the average normal value between
+     * two quads - ie along the side of an object
+     *
+     * @param coords The coordinates to generate from
+     * @param p The centre point
+     * @param p1 The first point of the first side
+     * @param p2 The middle, shared side point
+     * @param p3 The last point of the second side
+     * @return The averaged vector
+     */
+    private Vector3f calcSideAverageNormal(int w, int p,
+                                           int w1, int p1,
+                                           int w2, int p2,
+                                           int w3, int p3)
+    {
+        Vector3f norm;
+        float x, y, z;
+
+        // Normal first for the previous quad
+        norm = createFaceNormal(patchCoordinates, w, p, w1, p1, w2, p2);
+        x = norm.x;
+        y = norm.y;
+        z = norm.z;
+
+        // Normal for the next quad
+        norm = createFaceNormal(patchCoordinates, w, p, w2, p2, w3, p3);
+
+        // create the average of each compoenent for the final normal
+        norm.x = (norm.x + x) / 2;
+        norm.y = (norm.y + y) / 2;
+        norm.z = (norm.z + z) / 2;
+
+        norm.normalize();
+
+        return norm;
+    }
+
+    /**
+     * Convenience method to create quad average normal amongst four
+     * quads based around a common centre point (the one having the normal
+     * calculated).
+     *
+     * @param coords The coordinates to generate from
+     * @param p The centre point
+     * @param p1 shared point between first and last quad
+     * @param p2 shared point between first and second quad
+     * @param p3 shared point between second and third quad
+     * @param p4 shared point between third and fourth quad
+     * @return The averaged vector
+     */
+    private Vector3f calcQuadAverageNormal(int w, int p,
+                                           int w1, int p1,
+                                           int w2, int p2,
+                                           int w3, int p3,
+                                           int w4, int p4)
+    {
+        Vector3f norm;
+        float x, y, z;
+
+        // Normal first for quads 1 & 2
+        norm = createFaceNormal(patchCoordinates, w, p, w2, p2, w1, p1);
+        x = norm.x;
+        y = norm.y;
+        z = norm.z;
+
+        // Normal for the quads 2 & 3
+        norm = createFaceNormal(patchCoordinates, w, p, w2, p2, w3, p3);
+
+        x += norm.x;
+        y += norm.y;
+        z += norm.z;
+
+        // Normal for quads 3 & 4
+        norm = createFaceNormal(patchCoordinates, w, p, w3, p3, w4, p4);
+
+        x += norm.x;
+        y += norm.y;
+        z += norm.z;
+
+        // Normal for quads 1 & 4
+        norm = createFaceNormal(patchCoordinates, w, p, w4, p4, w1, p1);
+
+        // create the average of each compoenent for the final normal
+        norm.x = (norm.x + x) / 4;
+        norm.y = (norm.y + y) / 4;
+        norm.z = (norm.z + z) / 4;
+
+        norm.normalize();
+
+        return norm;
+    }
+
+    /**
+     * Regenerate the texture coordinate points.
+     * Assumes regenerateBase has been called before this
+     */
+    private final void regenerateTexcoords()
+    {
+        if(!texCoordsChanged)
+            return;
+
+        texCoordsChanged = false;
+
+        numTexcoordValues = facetCount * facetCount * 2;
+
+        if((patchTexcoords == null) ||
+           (numTexcoordValues > patchTexcoords.length))
+        {
+            patchTexcoords = new float[numTexcoordValues];
+        }
+
+        float d = 0;
+        float w = 0;
+        float width_inc = 1.0f / (facetCount - 1);
+        float depth_inc = 1.0f / (facetCount - 1);
+
+        int count = 0;
+
+        for(int i = 0; i < facetCount; i++)
+        {
+            for(int j = 0;  j < facetCount; j++)
+            {
+                patchTexcoords[count++] = w;
+                patchTexcoords[count++] = d;
+
+                w += width_inc;
+            }
+
+            d += depth_inc;
+            w = 0;
+        }
     }
 }
