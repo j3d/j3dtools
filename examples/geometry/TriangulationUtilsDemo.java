a876e7427ee5d61bd74904bc506d65ebe0751733
justin <justin>
2002-12-13 23:27:39 +0000

    JC: Added test for indexed triangle code

--- examples/geometry/TriangulationUtilsDemo.java
+++ examples/geometry/TriangulationUtilsDemo.java
@@ -21,7 +21,7 @@ import org.j3d.geom.*;
  * <p>
  *
  * @author Justin Couch
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class TriangulationUtilsDemo
 {
@@ -48,17 +48,17 @@ public class TriangulationUtilsDemo
         float[] normal = { 0, 0, 1 };
         int[] output = new int[30];
 
-for(int i = 0; i < coords.length; )
-{
-    System.out.print(i);
-    System.out.print(" ");
-    System.out.print(coords[i++]);
-    System.out.print(" ");
-    System.out.print(coords[i++]);
-    System.out.print(" ");
-    System.out.print(coords[i++]);
-    System.out.println();
-}
+        for(int i = 0; i < coords.length; )
+        {
+            System.out.print(i);
+            System.out.print(" ");
+            System.out.print(coords[i++]);
+            System.out.print(" ");
+            System.out.print(coords[i++]);
+            System.out.print(" ");
+            System.out.print(coords[i++]);
+            System.out.println();
+        }
 
         int num = triangulator.triangulateConcavePolygon(coords,
                                                          0,
@@ -95,6 +95,49 @@ for(int i = 0; i < coords.length; )
             System.out.print(coords[output[i * 3 + 2] + 1]);
             System.out.print(" ");
             System.out.print(coords[output[i * 3 + 2] + 2]);
+            System.out.println();
+        }
+
+        System.out.println("Trying indexed version");
+
+        int[] indexes = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+        num = triangulator.triangulateConcavePolygon(coords,
+                                                     0,
+                                                     12,
+                                                     indexes,
+                                                     output,
+                                                     normal);
+
+        System.out.println("number of triangles = " + num);
+
+        for(int i = 0; i < num; i++)
+        {
+            System.out.print(i);
+            System.out.print(": ");
+            System.out.print(output[i * 3]);
+            System.out.print(" ");
+            System.out.print(output[i * 3 + 1]);
+            System.out.print(" ");
+            System.out.print(output[i * 3 + 2]);
+            System.out.print(" c ");
+            System.out.print(coords[output[i] * 3]);
+            System.out.print(" ");
+            System.out.print(coords[output[i] * 3 + 1]);
+            System.out.print(" ");
+            System.out.print(coords[output[i] * 3 + 2]);
+            System.out.print(", ");
+            System.out.print(coords[output[i + 1] * 3]);
+            System.out.print(" ");
+            System.out.print(coords[output[i + 1] * 3 + 1]);
+            System.out.print(" ");
+            System.out.print(coords[output[i + 1] * 3 + 2]);
+            System.out.print(", ");
+            System.out.print(coords[output[i + 2] * 3]);
+            System.out.print(" ");
+            System.out.print(coords[output[i + 2] * 3 + 1]);
+            System.out.print(" ");
+            System.out.print(coords[output[i + 2] * 3 + 2]);
             System.out.println();
         }
     }
