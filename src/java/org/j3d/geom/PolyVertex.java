c3bd78539af1ee30e70cfe23b0bf31e0e43ef793
justin <justin>
2002-12-13 23:31:39 +0000

    JC: Internal convenience class that reprsents an individual vertex of a polygon

---
+++ src/java/org/j3d/geom/PolyVertex.java
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ *                          J3D.org Copyright (c) 2000
+ *                                Java Source
+ *
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
+ *
+ ****************************************************************************/
+
+package org.j3d.geom;
+
+// Standard imports
+import javax.vecmath.Point3d;
+
+// Application specific imports
+// none
+
+/**
+ * Utility class representing a segment of a polygon used in the tesselation
+ * routines.
+ *
+ * @author Justin Couch
+ * @version $Revision: 1.1 $
+ */
+class PolyVertex {
+
+    /** the index from the source array this vertex corresponds to */
+    public int vertexIndex;
+
+    /** X coordinate of the vertex */
+    public float x;
+
+    /** Y coordinate of the vertex */
+    public float y;
+
+    /** Z coordinate of the vertex */
+    public float z;
+
+    /** Pointer to the next segment */
+    public PolyVertex next;
+
+    /** Pointer to the previous segment */
+    public PolyVertex prev;
+}
