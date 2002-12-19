0d974329526dfa8e22fee07e1e39d5953b894de5
justin <justin>
2002-12-19 18:24:31 +0000

    JC: Added javadoc documentation for the methods

--- src/java/org/j3d/geom/particle/ParticleFunction.java
+++ src/java/org/j3d/geom/particle/ParticleFunction.java
@@ -10,17 +10,33 @@
 package org.j3d.geom.particle;
 
 /**
- * ParticleFunction is the basic interface for
- * functions that can modify the fields of a Particle.
- * Some movement functions will modify the force/energy of
- * a particle while others will convert the energy into
+ * ParticleFunction is the basic interface for functions that can modify the 
+ * fields of a Particle.
+ *
+ * A function may act on any of the fields of a particle, though typically it
+ * is on the size and position. Some movement functions will modify the 
+ * force/energy of a particle while others will convert the energy into
  * position deltas.
  *
  * @author Daniel Selman
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public interface ParticleFunction
 {
+    /**
+     * Notification that the system is about to do an update of the particles
+     * and to do any system-level initialisation.
+     *
+     * @param ps The particle system that is being updated
+     * @return true if this has done it's updating
+     */
     public boolean apply( Particle particle );
+
+    /**
+     * Apply this function to the given particle right now.
+     *
+     * @param particle The particle to apply the function to
+     * @return true if the particle 
+     */
     public boolean onUpdate( ParticleSystem ps );
 }
