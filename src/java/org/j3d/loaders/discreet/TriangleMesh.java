df0dada9930cd71661c8c3bef28bec1ec712e3eb
justin <justin>
2004-07-27 18:56:21 +0000

    JC: Changed the variable names to be consistent with the rest of the codebase - mostly use of plurals

--- src/java/org/j3d/loaders/discreet/TriangleMesh.java
+++ src/java/org/j3d/loaders/discreet/TriangleMesh.java
@@ -34,15 +34,15 @@ package org.j3d.loaders.discreet;
  * </pre>
  *
  * @author  Justin Couch
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class TriangleMesh
 {
     /** Vertex coordinates, flat style */
-    public float[] vertex;
+    public float[] vertices;
 
     /** The number of valid vertices in this mesh */
-    public int numVertex;
+    public int numVertices;
 
     /** 2D texture coordinates for this object */
     public float[] texCoords;
@@ -51,29 +51,36 @@ public class TriangleMesh
     public int numTexCoords;
 
     /** Index lists for each face */
-    public int[] face;
-
-    /** Index lists for the groups of faces that should be smooth shaded */
-    public int[] smoothgroup;
+    public int[] faces;
 
     /** Total number of valid faces. */
-    public int numFace;
+    public int numFaces;
+
+    /** Index lists for the groups of faces that should be smooth shaded */
+    public int[] smoothgroups;
 
     /** Listing of normals for each vertex */
-    public float[] normal;
+    public float[] normals;
 
     /** Listing of tangents for each vertex */
-    public float[] tangent;
+    public float[] tangents;
 
     /** Listing of binormals for each vertex */
-    public float[] binormal;
+    public float[] binormals;
 
     /** Local coordinate system reference (a 4x3 matrix)*/
     public float[] localCoords;
 
+    /** Listing of the material groups needed */
+    public MaterialData[] materials;
+
     /** Number of valid material groups available */
     public int numMaterials;
 
-    /** Listing of the material groups needed */
-    public MaterialData[] materials;
+    /**
+     * Names of the materials used for a cubic environment map. This will be
+     * mapped in the order: front, back, left, right, top, bottom. Will be null
+     * if nothing declared.
+     */
+    public String[] boxMapMaterials;
 }
