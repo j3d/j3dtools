99a8f539ddda7bc8939c65ea65dc64ded7854ee2
justin <justin>
2008-05-22 17:39:12 +0000

    JC: Found a heap of files from some broken app that generates end_solid rather than endsolid. Have the code magically deal with this to generate a correct file output

--- src/java/org/j3d/loaders/stl/STLASCIIParser.java
+++ src/java/org/j3d/loaders/stl/STLASCIIParser.java
@@ -24,128 +24,128 @@ import javax.swing.ProgressMonitorInputStream;
  * @see STLLoader
  * @author  Dipl. Ing. Paul Szawlowski -
  *          University of Vienna, Dept of Medical Computer Sciences
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 class STLASCIIParser extends STLParser
 {
     private BufferedReader  itsReader;
     private StreamTokenizer itsTokenizer;
 
-    public STLASCIIParser( )
+    public STLASCIIParser()
     {
     }
 
-    public void close( ) throws IOException
+    public void close() throws IOException
     {
-        if( itsReader != null )
+        if(itsReader != null)
         {
-            itsReader.close( );
+            itsReader.close();
         }
     }
 
-    public boolean getNextFacet( final double[ ] normal, double[ ][ ] vertices )
-    throws IOException
+    public boolean getNextFacet(double[ ] normal, double[ ][ ] vertices)
+        throws IOException
     {
-        int type = itsTokenizer.nextToken( );
-        if( type  == StreamTokenizer.TT_EOF )
+        int type = itsTokenizer.nextToken();
+        if(type  == StreamTokenizer.TT_EOF)
         {
-            close( );
-            throw new IOException( "Unexpected EOF" );
+            close();
+            throw new IOException("Unexpected EOF");
         }
-        else if( type == StreamTokenizer.TT_WORD )
+        else if(type == StreamTokenizer.TT_WORD)
         {
-            // check if end of object is reached ( 's' in "endsolid" )
-            if( itsTokenizer.sval.indexOf( "s" ) >= 0 )
+            // check if end of object is reached ('s' in "endsolid")
+            if(itsTokenizer.sval.indexOf("s") >= 0)
             {
-                skipObjectName( itsTokenizer );
+                skipObjectName(itsTokenizer);
                 // skip "solid" keyword
-                type = itsTokenizer.nextToken( );
-                if( type == StreamTokenizer.TT_EOF )
+                type = itsTokenizer.nextToken();
+                if(type == StreamTokenizer.TT_EOF)
                 {
                     // eof is reached, i. e. no more objects in file
-                    close( );
+                    close();
                     return false;
                 }
-                skipObjectName( itsTokenizer );
+                skipObjectName(itsTokenizer);
             }
             // push back x coordinate of facet normal
             else
             {
-                itsTokenizer.pushBack( );
+                itsTokenizer.pushBack();
             }
-            readVector( itsTokenizer, normal );
-            for( int i = 0; i < 3; i ++ )
+            readVector(itsTokenizer, normal);
+            for(int i = 0; i < 3; i ++)
             {
-                readVector( itsTokenizer, vertices[ i ] );
+                readVector(itsTokenizer, vertices[ i ]);
             }
             return true;
         }
         else
         {
-            close( );
-            throw new IOException( "Unexpected data found");
+            close();
+            throw new IOException("Unexpected data found");
         }
     }
 
 
-    public boolean parse( final URL url, final Component parentComponent )
-    throws InterruptedIOException, IOException
+    public boolean parse(URL url, Component parentComponent)
+        throws InterruptedIOException, IOException
     {
         InputStream stream = null;
         try
         {
-            stream = url.openStream( );
+            stream = url.openStream();
         }
-        catch( IOException e )
+        catch(IOException e)
         {
-            if( stream != null )
+            if(stream != null)
             {
-                stream.close( );
+                stream.close();
             }
             throw e;
         }
         stream = new ProgressMonitorInputStream
         (
             parentComponent,
-            "analyzing " + url.toString( ),
+            "analyzing " + url.toString(),
             stream
-        );
+       );
         BufferedReader reader =
-            new BufferedReader( new InputStreamReader( stream ) );
+            new BufferedReader(new InputStreamReader(stream));
         boolean isAscii = false;
         try
         {
-            isAscii = parse( reader );
+            isAscii = parse(reader);
         }
         finally
         {
-            reader.close( );
+            reader.close();
         }
-        if( isAscii )
+        if(isAscii)
         {
             try
             {
-                stream = url.openStream( );
+                stream = url.openStream();
             }
-            catch( IOException e )
+            catch(IOException e)
             {
-                stream.close( );
+                stream.close();
                 throw e;
             }
             stream = new ProgressMonitorInputStream
             (
                 parentComponent,
-                "parsing " + url.toString( ),
+                "parsing " + url.toString(),
                 stream
-            );
-            reader = new BufferedReader( new InputStreamReader( stream ) );
+           );
+            reader = new BufferedReader(new InputStreamReader(stream));
             try
             {
-                configureTokenizer( reader );
+                configureTokenizer(reader);
             }
-            catch( IOException e )
+            catch(IOException e)
             {
-                reader.close( );
+                reader.close();
                 throw e;
             }
             itsReader = reader;
@@ -153,57 +153,57 @@ class STLASCIIParser extends STLParser
         return isAscii;
     }
 
-    public boolean parse( final URL url )
-    throws IOException
+    public boolean parse(URL url)
+        throws IOException
     {
         InputStream stream = null;
         try
         {
-            stream = url.openStream( );
+            stream = url.openStream();
         }
-        catch( IOException e )
+        catch(IOException e)
         {
-            if( stream != null )
+            if(stream != null)
             {
-                stream.close( );
+                stream.close();
             }
             throw e;
         }
         BufferedReader reader =
-            new BufferedReader( new InputStreamReader( stream ) );
+            new BufferedReader(new InputStreamReader(stream));
         boolean isAscii = false;
         try
         {
-            isAscii = parse( reader );
+            isAscii = parse(reader);
         }
-        catch( InterruptedIOException e )
+        catch(InterruptedIOException e)
         {
             // should never happen
-            e.printStackTrace( );
+            e.printStackTrace();
         }
         finally
         {
-            reader.close( );
+            reader.close();
         }
-        if( isAscii )
+        if(isAscii)
         {
             try
             {
-                stream = url.openStream( );
+                stream = url.openStream();
             }
-            catch( IOException e )
+            catch(IOException e)
             {
-                stream.close( );
+                stream.close();
                 throw e;
             }
-            reader = new BufferedReader( new InputStreamReader( stream ) );
+            reader = new BufferedReader(new InputStreamReader(stream));
             try
             {
-                configureTokenizer( reader );
+                configureTokenizer(reader);
             }
-            catch( IOException e )
+            catch(IOException e)
             {
-                reader.close( );
+                reader.close();
                 throw e;
             }
             itsReader = reader;
@@ -211,60 +211,64 @@ class STLASCIIParser extends STLParser
         return isAscii;
     }
 
-    private boolean parse( final BufferedReader reader )
-    throws InterruptedIOException, IOException
+    private boolean parse(BufferedReader reader)
+        throws InterruptedIOException, IOException
     {
         int numOfObjects = 0;
         int numOfFacets = 0;
-        final ArrayList facetsPerObject = new ArrayList( 10 );
-        final ArrayList names = new ArrayList( 10 );
+        ArrayList facetsPerObject = new ArrayList(10);
+        ArrayList names = new ArrayList(10);
         boolean isAscii = true;
-        String line = reader.readLine( );
+        String line = reader.readLine();
 
         // check if ASCII format
-        if( line.indexOf( "solid" ) < 0 )
+        if(line.indexOf("solid") < 0)
         {
             return false;
         }
 
-        while( line != null )
+        while(line != null)
         {
-            if( line.indexOf( "facet" ) >= 0 )
+            if(line.indexOf("facet") >= 0)
             {
                 numOfFacets ++;
                 // skip next 6 lines:
                 // outer loop, 3 * vertex, endloop, endfacet
-                for( int i = 0; i < 6; i ++ )
+                for(int i = 0; i < 6; i ++)
                 {
-                    reader.readLine( );
+                    reader.readLine();
                 }
             }
             // watch order of if: solid contained also in endsolid
-            else if( line.indexOf( "endsolid" ) >= 0 )
+            // JC: We have found a lot of badly formatted STL files generated
+            // from some program that incorrectly end a solid object with a
+            // space between end and solid. Deal with that here.
+            else if((line.indexOf("endsolid") >= 0) ||
+                    (line.indexOf("end solid") >= 0))
             {
-                facetsPerObject.add( new Integer( numOfFacets ) );
+                facetsPerObject.add(new Integer(numOfFacets));
                 numOfFacets = 0;
                 numOfObjects ++;
             }
-            else if( line.indexOf( "solid" ) >= 0 )
+            else if(line.indexOf("solid") >= 0)
             {
-                names.add( line.substring( 6 ) );
+                names.add(line.substring(6));
             }
             else
             {
                 // format not correct
                 return false;
             }
-            line = reader.readLine( );
+            line = reader.readLine();
         }
         itsNumOfObjects = numOfObjects;
         itsNumOfFacets = new int[ numOfObjects ];
         itsNames = new String[ numOfObjects ];
-        for( int i = 0; i < numOfObjects; i ++ )
+        for(int i = 0; i < numOfObjects; i ++)
         {
-            final Integer num = ( Integer )facetsPerObject.get( i );
-            itsNumOfFacets[ i ] = num.intValue( );
-            itsNames[ i ] = ( String )names.get( i );
+            Integer num = (Integer)facetsPerObject.get(i);
+            itsNumOfFacets[ i ] = num.intValue();
+            itsNames[ i ] = (String)names.get(i);
         }
         return true;
     }
@@ -272,71 +276,67 @@ class STLASCIIParser extends STLParser
     /**
      * Set the <code>BufferedReader</code> object for reading the facet data.
      */
-    private void configureTokenizer( final BufferedReader reader )
-    throws IOException
+    private void configureTokenizer(BufferedReader reader)
+        throws IOException
     {
-        reader.readLine( );
-        itsTokenizer = new StreamTokenizer( reader );
-        itsTokenizer.resetSyntax( );
+        reader.readLine();
+        itsTokenizer = new StreamTokenizer(reader);
+        itsTokenizer.resetSyntax();
         //configure StreamTokenizer:
         // only numbers shall be parsed
-        itsTokenizer.wordChars( '0', '9' );
+        itsTokenizer.wordChars('0', '9');
         // works only if 'e' is not used for exponent !
-        itsTokenizer.wordChars( 'E', 'E' );
-        itsTokenizer.wordChars( '+', '+' );
-        itsTokenizer.wordChars( '-', '-' );
-        itsTokenizer.wordChars( '.', '.' );
+        itsTokenizer.wordChars('E', 'E');
+        itsTokenizer.wordChars('+', '+');
+        itsTokenizer.wordChars('-', '-');
+        itsTokenizer.wordChars('.', '.');
         // find solid and endsolid keywords
-        itsTokenizer.wordChars( 's', 's' );
+        itsTokenizer.wordChars('s', 's');
         // all other characters (only lower case allowed except for model
         // name - model name will be treated in special case) shall be
         //treated as whitespace
-        itsTokenizer.whitespaceChars( 0, ' ' );
-        itsTokenizer.whitespaceChars( 'a', 'r' );
-        itsTokenizer.whitespaceChars( 't', 'z' );
+        itsTokenizer.whitespaceChars(0, ' ');
+        itsTokenizer.whitespaceChars('a', 'r');
+        itsTokenizer.whitespaceChars('t', 'z');
     }
 
-    private void readVector
-    (
-        final StreamTokenizer   tokenizer,
-        final double[ ]         vector
-    )
-    throws IOException
+    private void readVector(StreamTokenizer tokenizer, double[] vector)
+        throws IOException
     {
-        for( int i = 0; i < 3; i ++ )
+        for(int i = 0; i < 3; i ++)
         {
-            final int type = tokenizer.nextToken( );
-            if( type == StreamTokenizer.TT_WORD )
+            int type = tokenizer.nextToken();
+            if(type == StreamTokenizer.TT_WORD)
             {
                 try
                 {
-                    vector[ i ] = Double.parseDouble( tokenizer.sval );
+                    vector[ i ] = Double.parseDouble(tokenizer.sval);
                 }
-                catch( NumberFormatException e )
+                catch(NumberFormatException e)
                 {
-                    throw new IOException( "Unexpected data found");
+                    throw new IOException("Unexpected data found");
                 }
             }
-            else if( type == StreamTokenizer.TT_EOF )
+            else if(type == StreamTokenizer.TT_EOF)
             {
-                throw new IOException( "Unexpected EOF");
+                throw new IOException("Unexpected EOF");
             }
             else
             {
-                throw new IOException( "Unexpected data found." );
+                throw new IOException("Unexpected data found.");
             }
         }
     }
 
-    private void skipObjectName( final StreamTokenizer tokenizer )
-    throws IOException
+    private void skipObjectName(StreamTokenizer tokenizer)
+        throws IOException
     {
-        itsTokenizer.eolIsSignificant( true );
+        itsTokenizer.eolIsSignificant(true);
         int type = 0;
-        while( type != StreamTokenizer.TT_EOL )
+        while(type != StreamTokenizer.TT_EOL)
         {
-            type = itsTokenizer.nextToken( );
+            type = itsTokenizer.nextToken();
         }
-        itsTokenizer.eolIsSignificant( false );
+        itsTokenizer.eolIsSignificant(false);
     }
 }
